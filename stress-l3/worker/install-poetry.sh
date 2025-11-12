4dd77557528a7978b7ab615c24ab3338b01b41f3
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2025-11-12 15:01:32 +0100

    Create poetry installation script

---
+++ stress-l3/worker/install-poetry.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Install Poetry using official installer
+
+echo "Installing Poetry..."
+
+sudo bash -c 'export PATH="$HOME/.local/bin:$PATH" && curl -sSL https://install.python-poetry.org | python3 -'
+
+# Add poetry to PATH in bashrc
+if ! grep -q '\.local/bin' "$HOME/.bashrc"; then
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
+fi
+
+export PATH="$HOME/.local/bin:$PATH"
+
+echo "Poetry installed."
+
